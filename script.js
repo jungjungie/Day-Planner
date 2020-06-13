7614bd38590f19de396e1c8603903ac3e37ca22b
Esther Min <estherjmin@gmail.com>
2020-06-13 10:13:29 -0400

    color coded time blocks based on past, current, future

--- script.js
+++ script.js
@@ -23,28 +23,29 @@ $(".saveBtn").on("click", function () {
 currentTime = parseInt(currentTime);
 
 // console.log($(".col-1").text())
-console.log($(".time"));
+// console.log($(".time"));
 
 // var timeArr = document.querySelectorAll(".time");
 // console.log(timeArr);
 
 var timeArr = $(".time");
 var eventBlockArr = $("textarea");
+var pastTimeArr = [];
 
 for (var i=0; i < timeArr.length; i++) {
 
     var timeblock = timeArr[i].textContent;
     // 9 , 10, 11 , 12 , 1 , 2 , 3 , 4 , 5
 
-    if (timeblock < currentTime) {
-        eventBlockArr.eq(i).css("background", "green");
-    }
-
     if (timeblock == currentTime) {
-        eventBlockArr.eq(i).css("background", "yellow");
+        // Current time block is red
+        eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "1px solid #ff5349", "border-bottom": "1px solid #ff5349"});
+        eventBlockArr.eq(i).addClass("lead");
+        
+        // Previous time blocks are gray
+        pastTimeArr = eventBlockArr.slice(0, i);
+        for (j=0; j < pastTimeArr.length; j++) {
+            pastTimeArr.eq(j).css("background", "#f2f2f2");
+        } 
     }
 }
-    
-// }
-// current time block is green
-// future time blocks are yellow
