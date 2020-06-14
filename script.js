90e91ecc50cfc202d2d71f8f2d4eb486de77b67c
Esther Min <estherjmin@gmail.com>
2020-06-13 23:18:12 -0400

    Set saved events to go to localStorage

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 var today = moment().format('dddd, MMMM Do');
-var currentTime = moment().format('LT');
+// var currentTime = moment().format('LT');
+var currentTime = moment().format('h A');
+var timeArr = $(".time");
+var eventBlockArr = $("textarea");
+var saveBtnArr = $(".saveBtn");
+// var pastTimeArr = [];
+// var storedItems = [];
 
+// Sets current date at top of page
 $("#currentDay").text(today);
 
-// add events to save buttons
+// Series of events when save button is clicked
 $(".saveBtn").on("click", function () {
+    // console.log(event); 
 
-    // When save is clicked, event text gets saved in localstorage
-    localStorage.setItem("event", $(".event").text);
+    // Grabs index of save button that was clicked
+    var clickedIndex = saveBtnArr.index($(this));
+    clickedIndex = parseInt(clickedIndex);
 
-    console.log(event); 
-    console.log(event.target.value); 
+    // Grabs text of event that is being saved
+    var clickedEvent = eventBlockArr[clickedIndex].value;
 
-    console.log(window);
-    console.log($("#text10").text());
+    // Grabs timeslot where event was saved
+    var clickedTime = timeArr[clickedIndex].textContent;
+
+    // Saved items go to localStorage
+    var storedItems = JSON.parse(localStorage.getItem("key")) || [];
+    storedItems.push([{Time: clickedTime, Event: clickedEvent}]);
+    storedItems = JSON.stringify(storedItems);
+
+    localStorage.setItem("key", storedItems);
+
 })
     
     // Save items are retrived from localstorage and displayed on screen
 
 // past time events are left gray
 
-currentTime = parseInt(currentTime);
+// currentTime = parseInt(currentTime);
 
 // console.log($(".col-1").text())
 // console.log($(".time"));
@@ -28,24 +45,43 @@ currentTime = parseInt(currentTime);
 // var timeArr = document.querySelectorAll(".time");
 // console.log(timeArr);
 
-var timeArr = $(".time");
-var eventBlockArr = $("textarea");
-var pastTimeArr = [];
 
+// Time block colors
+// for (var i=0; i < timeArr.length; i++) {
+
+//     var timeblock = timeArr[i].textContent;
+
+//     if (timeblock == currentTime) {
+//         // Current time block is red
+//         eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "2px solid #ff5349", "border-bottom": "2px solid #ff5349"});
+//         eventBlockArr.eq(i).addClass("lead");
+        
+//         // Past time blocks are gray
+//         pastTimeArr = eventBlockArr.slice(0, i);
+//         for (j=0; j < pastTimeArr.length; j++) {
+//             pastTimeArr.eq(j).css("background", "#f2f2f2");
+//         } 
+//     }
+// }
+
+// Time block colors
 for (var i=0; i < timeArr.length; i++) {
 
     var timeblock = timeArr[i].textContent;
-    // 9 , 10, 11 , 12 , 1 , 2 , 3 , 4 , 5
+    timeblock = moment(timeblock, 'h A');
+    // console.log(timeblock);
 
-    if (timeblock == currentTime) {
+    if (timeblock._i == currentTime) {
         // Current time block is red
-        eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "1px solid #ff5349", "border-bottom": "1px solid #ff5349"});
+        eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "2px solid #ff5349", "border-bottom": "2px solid #ff5349"});
         eventBlockArr.eq(i).addClass("lead");
         
-        // Previous time blocks are gray
-        pastTimeArr = eventBlockArr.slice(0, i);
-        for (j=0; j < pastTimeArr.length; j++) {
-            pastTimeArr.eq(j).css("background", "#f2f2f2");
-        } 
+    } 
+    else if (timeblock._i < currentTime) {
+        eventBlockArr.eq(i).css("background", "#f2f2f2")
+    } else {
+        eventBlockArr.eq(i).css("background", "#b5e2ff")
     }
 }
+
+// DON'T DELETE LINE 61
