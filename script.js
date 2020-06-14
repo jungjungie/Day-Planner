07c9eb35f05f46dffa4f52f01fd34bc2f030da36
Esther Min <estherjmin@gmail.com>
2020-06-14 16:03:02 -0400

    Fixed bug with colors for timeblocks

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var today = moment().format('dddd, MMMM Do');
 // var currentTime = moment().format('LT');
-var currentTime = moment().format('h A');
+var currentTime = moment().hour();
 var timeArr = $(".time");
 var eventBlockArr = $("textarea");
 var saveBtnArr = $(".saveBtn");
@@ -18,11 +18,11 @@ $(".saveBtn").on("click", function () {
     var clickedIndex = saveBtnArr.index($(this));
     clickedIndex = parseInt(clickedIndex);
 
-    // Grabs text of event that is being saved
-    var clickedEvent = eventBlockArr[clickedIndex].value;
-
-    // Grabs timeslot where event was saved
-    var clickedTime = timeArr[clickedIndex].textContent;
+    // Grabs text & timeslot of event that is being saved
+    var clickedEvent = $(eventBlockArr[clickedIndex]).val();
+    var clickedTime = $(timeArr[clickedIndex]).text();
+    console.log(clickedEvent);
+    console.log(clickedTime);
 
     // Saved items go to localStorage
     storedItems = JSON.parse(localStorage.getItem("key")) || [];
@@ -33,9 +33,9 @@ $(".saveBtn").on("click", function () {
 
 function viewSavedPlans() {
     // Grabs saved items from localStorage
-    storedItems = JSON.parse(localStorage.getItem("key"));
+    storedItems = JSON.parse(localStorage.getItem("key")) || [];
     console.log(storedItems);
-    console.log(storedItems[0].Time);
+    // console.log(storedItems[0].Time);
 
     // If saved item key (Time) = a time slot in timeArr, event gets populated into that event slot
     for (var i=0; i < storedItems.length; i++) {
@@ -48,53 +48,26 @@ function viewSavedPlans() {
 }
 viewSavedPlans();
 
-// past time events are left gray
-
-// currentTime = parseInt(currentTime);
-
-// console.log($(".col-1").text())
-// console.log($(".time"));
-
-// var timeArr = document.querySelectorAll(".time");
-// console.log(timeArr);
-
-
-// Time block colors
-// for (var i=0; i < timeArr.length; i++) {
-
-//     var timeblock = timeArr[i].textContent;
-
-//     if (timeblock == currentTime) {
-//         // Current time block is red
-//         eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "2px solid #ff5349", "border-bottom": "2px solid #ff5349"});
-//         eventBlockArr.eq(i).addClass("lead");
-        
-//         // Past time blocks are gray
-//         pastTimeArr = eventBlockArr.slice(0, i);
-//         for (j=0; j < pastTimeArr.length; j++) {
-//             pastTimeArr.eq(j).css("background", "#f2f2f2");
-//         } 
-//     }
-// }
-
 // Time block colors
 for (var i=0; i < timeArr.length; i++) {
 
-    var timeblock = timeArr[i].textContent;
-    timeblock = moment(timeblock, 'h A');
-    // console.log(timeblock);
+    const hr = i + 9;
 
-    if (timeblock._i == currentTime) {
+    var timeblock = timeArr[i].textContent;
+    //timeblock = moment(timeblock, 'h A');
+    //console.log(timeblock);
+    console.log(`hour: ${hr} timeblock: ${timeblock}`)
+    if (hr == currentTime) {
         // Current time block is red
         eventBlockArr.eq(i).css({"background": "#FFE5E4", "color": "#ff5349", "border-top": "2px solid #ff5349", "border-bottom": "2px solid #ff5349"});
         eventBlockArr.eq(i).addClass("lead");
-        
     } 
-    else if (timeblock._i < currentTime) {
+    // If past time, color is gray
+    else if (hr < currentTime) {
         eventBlockArr.eq(i).css("background", "#f2f2f2")
-    } else {
+    } 
+    // If upcoming time, color is blue
+    else {
         eventBlockArr.eq(i).css("background", "#b5e2ff")
     }
 }
-
-// DON'T DELETE LINE 53
